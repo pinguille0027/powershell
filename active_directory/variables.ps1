bb8e79596ee5f842d4e0bcc1c6b514c1250aab0b
pinguille0027 <guillermo.orio27@gmail.com>
2024-02-08 14:28:23 +0100

    grupos primera fase

--- active_directory/variables.ps1
+++ active_directory/variables.ps1
@@ -1,1 +1,3 @@
 $listadoOUs=".\Unidades-Organizativas-Xilgaro-XX.csv"
+$rutaBasica="OU=Sede-Vigo,DC=Xilgaro-dm,DC=local"
+$gruposPrincipales=@(@{nombre="X-Usuarios"; ruta="OU=Usuarios,$rutaBasica"})
